1c4d977ba7b9edb6219564b3ac4f7ffc9cac6176
Naman <namanvarshneyofficial@gmail.com>
2021-12-08 13:12:05 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import styled from 'styled-components';
+import Home from './components/Home';
 
 function App() {
     return (
         <div className="App">
-            Naman Varshney
+            <Header />
+            <Home />
         </div>
     );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" alt="Disney+ logo" />
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImg src="" alt="User Image" />
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.nav`
+    height : 60px;
+    background : #090b13;
+    display : flex;
+    align-items : center;
+    padding : 0 36px;
+`
+
+const Logo = styled.img`
+    width : 80px;
+
+`
+const NavMenu = styled.div`
+    display : flex;
+    flex:1;
+
+    a{
+        display : flex;
+        align-items : center;
+        padding : 0 12px;
+        cursor : pointer;
+
+        img{
+            height : 20px;
+            padding : 0 4px;
+        }
+
+        span{
+            font-size : 13px;
+            letter-spacing : 1.5px;
+            position : relative;
+
+            &:after{
+                content:"";
+                height : 2px;
+                background : white;
+                position : absolute;
+                left : 0;
+                right : 0;
+                bottom : -6px;
+                opacity : 0;       
+                transform-origin : left center;
+                transition : all 250ms ease-in-out 0s; 
+                transform : scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform : scaleX(1);
+                opacity : 1;
+            }
+        }
+    }
+`
+const UserImg = styled.img`
+    height : 48px;
+    width : 48px;
+    border-radius : 50%;
+    cursor : pointer;
+`
